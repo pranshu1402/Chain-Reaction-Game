8f021d22c7872c6a401e93b9776625a8595f9c7d
pranshu1402 <pranshu.ghps@gmail.com>
2020-06-11 18:41:35 +0530

    Updating block styles V2

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,12 +2,11 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-    return (
-        <div className="app-footer">
-            Footer
-            {/* Keep it simple with a contact strip and minimal links (link to repo also will be added) */}
-        </div>
-    );
+	return (
+		<div className='app-footer'>
+			{/* Footer: Keep it simple with a contact strip and minimal links (link to repo also will be added) */}
+		</div>
+	);
 };
 
 export default Footer;
